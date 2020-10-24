a98eacc9903ef953178ce020075e43b3826a5ffc
FloreauLuca <lucafloreau@akeonet.com>
2020-10-24 13:33:43 +0200

    Waves working

--- SecretsWeapon/Assets/Scripts/EnemiesSpawner.cs
+++ SecretsWeapon/Assets/Scripts/EnemiesSpawner.cs
@@ -1,20 +1,73 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
+[Serializable]
+struct Wave
+{
+    public float totalEnnemies;
+    public int enemiesGroup;
+    public float coolDown;
+    public GameObject prefab;
+    public Transform spawnPoint;
+    public SpriteRenderer waveFinish;
+}
 
 public class EnemiesSpawner : MonoBehaviour
 {
     private GameManager gameManager;
+    [SerializeField] private int currentWaveIndex = 0;
+    [SerializeField] private List<Wave> waves;
+
+
+    
 
     // Start is called before the first frame update
     void Start()
     {
         gameManager = FindObjectOfType<GameManager>();
+        foreach (Wave wave in waves) {
+            wave.waveFinish.enabled = false;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void StartNewWave()
+    {
+        if (waves.Count >= currentWaveIndex && currentWaveIndex > 0)
+        {
+            waves[currentWaveIndex - 1].waveFinish.enabled = true;
+        }
+        if (waves.Count > currentWaveIndex)
+        {
+            StartCoroutine(LaunchWave(waves[currentWaveIndex]));
+        }
+    }
+
+    IEnumerator LaunchWave(Wave wave)
+    {
+        int currentEnemyCount = 0;
+        while (currentEnemyCount < wave.totalEnnemies) {
+            yield return new WaitForSeconds(wave.coolDown);
+            for (int i = 0; i < wave.enemiesGroup; i++)
+            {
+                Instantiate(wave.prefab, (Vector2)wave.spawnPoint.position + Vector2.one * i, Quaternion.identity);
+                currentEnemyCount++;
+            }
+        }
+        currentWaveIndex++;
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.CompareTag("Player"))
+        {
+            StartNewWave();
+        }
     }
 }
--- SecretsWeapon/Assets/Scripts/Enemy.cs
+++ SecretsWeapon/Assets/Scripts/Enemy.cs
@@ -10,17 +10,29 @@ public class Enemy : MonoBehaviour
 
     private SpriteRenderer spriteRenderer;
 
+    private Player player;
+
+    [Header("Mouvement")]
+    private Rigidbody2D rigidbody;
+    [SerializeField] private float speed = 5.0f;
+    [SerializeField] private int angleOffset;
+
     // Start is called before the first frame update
     void Start()
     {
         gameManager = FindObjectOfType<GameManager>();
         spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        player = FindObjectOfType<Player>();
+        rigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 dir = player.transform.position - transform.position;
+        var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg + angleOffset;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        rigidbody.velocity = transform.up * speed;
     }
 
     public void Hit(int damage)
